9ab8d7fcf1b52cd789bbb4c18c650e00c48fbebf
Rogu <116495454+rgrgogu@users.noreply.github.com>
2024-03-15 20:22:02 +0800

    first commit

---
+++ src/components/footer/FooterRegistration.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="fixed bottom-0 w-full sm:h-[50%] md:h-[30%] lg:h-[22%]">
+    <img
+      src="../../assets/imgs/FooterReg.png"
+      alt="Image description"
+      class="w-full h-full object-cover brightness-50"
+    />
+    <div
+      class="grid md:grid-cols-2 lg:grid-cols-3 justify-between my-[50px] sm:mx-[20px] lg:mx-[50px] gap-5 items-start absolute inset-0"
+    >
+      <div class="text-overlay text-white md:col-span-2 lg:col-span-1 sm:w-[70%] sm:mx-auto lg:w-full lg:mx-0">
+        <h1 class="font-bold sm:text-center md:text-left text-xl">
+            FOOTER CONTENT
+        </h1>
+        <h2 class="sm:text-justify md:text-left">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda inventore, ut est vel magnam culpa ullam dicta deleniti officia beatae maiores temporibus quidem veniam consequuntur architecto nostrum rerum eligendi veritatis.
+        </h2>
+      </div>
+      <div class="text-overlay text-center  text-white flex flex-col">
+        <h2 class="font-bold text-xl">FOOTER LINK</h2>
+        <router-link to="/">Link1</router-link>
+        <router-link to="/">Link2</router-link>
+        <router-link to="/">Link3</router-link>
+        <router-link to="/">Link4</router-link>
+      </div>
+      <div class="text-overlay text-white items-center flex flex-col ">
+        <h2 class="font-bold text-xl">FOOTER LINK</h2>
+        <router-link to="/">Link1</router-link>
+        <router-link to="/">Link2</router-link>
+        <router-link to="/">Link3</router-link>
+        <router-link to="/">Link4</router-link>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/components/navigations/Sidebar.vue
@@ -0,0 +1,388 @@
+<script setup lang="ts">
+const { show } = defineProps(["show"]);
+const emit = defineEmits(["close-sidebar"]);
+
+const handleClose = () => {
+  emit("close-sidebar");
+};
+</script>
+
+<template>
+  <transition name="fade">
+    <div
+      v-if="show"
+      @click="handleClose"
+      class="fixed inset-0 bg-black opacity-50 z-10"
+    ></div>
+  </transition>
+  <transition name="slide">
+    <div
+      v-if="show"
+      class="fixed left-0 top-0 h-screen sm:w-full md:w-[400px] bg-[#131C39] text-[131C39] z-10 rounded-xl"
+    >
+      <div class="h-1/4 flex justify-center bg-white rounded-tr-xl">
+        <img src="../../assets/imgs/logo.png" alt="" />
+      </div>
+      <button class="absolute top-4 right-4" @click="handleClose">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6 hover:scale-95"
+          fill="white"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M6 18L18 6M6 6l12 12"
+          ></path>
+        </svg>
+      </button>
+      <ul
+        class="px-5 flex flex-col pt-6 text-white font-bold text-[1.4rem] space-y-4 h-1/2"
+      >
+        <div>
+          <li class="">
+            <router-link
+              to="/"
+              @click="handleClose"
+              :class="{ 'sidebar-link-active': $route.path === '/' }"
+              exact
+              class="flex flex-row px-4 py-2 items-center rounded-md hover:bg-gray-700 hover:transition hover:duration-300 hover:opacity-100"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-8 w-8"
+                fill="white"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <rect
+                  x="3"
+                  y="3"
+                  width="8"
+                  height="8"
+                  stroke="currentColor"
+                  stroke-width="2"
+                />
+                <rect
+                  x="15"
+                  y="3"
+                  width="8"
+                  height="8"
+                  stroke="currentColor"
+                  stroke-width="2"
+                />
+                <rect
+                  x="3"
+                  y="15"
+                  width="8"
+                  height="8"
+                  stroke="currentColor"
+                  stroke-width="2"
+                />
+                <rect
+                  x="15"
+                  y="15"
+                  width="8"
+                  height="8"
+                  stroke="currentColor"
+                  stroke-width="2"
+                />
+              </svg>
+              <p class="mx-4 text-[18px]">DASHBOARD</p>
+            </router-link>
+          </li>
+        </div>
+        <div>
+          <li class="">
+            <router-link
+              to="/accountstate"
+              @click="handleClose"
+              active-class="sidebar-link-active"
+              class="flex flex-row items-center px-4 py-2 rounded-md hover:bg-gray-700 hover:transition hover:duration-300 hover:opacity-100"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-8 w-8"
+                fill="white"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <rect
+                  x="3"
+                  y="3"
+                  width="8"
+                  height="8"
+                  stroke="currentColor"
+                  stroke-width="2"
+                />
+                <rect
+                  x="15"
+                  y="3"
+                  width="8"
+                  height="8"
+                  stroke="currentColor"
+                  stroke-width="2"
+                />
+                <rect
+                  x="3"
+                  y="15"
+                  width="8"
+                  height="8"
+                  stroke="currentColor"
+                  stroke-width="2"
+                />
+                <rect
+                  x="15"
+                  y="15"
+                  width="8"
+                  height="8"
+                  stroke="currentColor"
+                  stroke-width="2"
+                />
+              </svg>
+              <p class="mx-4 text-[18px]">ACCOUNT STATE</p>
+            </router-link>
+          </li>
+        </div>
+        <div>
+          <li class="">
+            <router-link
+              to="/learn"
+              @click="handleClose"
+              active-class="sidebar-link-active"
+              class="flex flex-row items-center px-4 py-2 rounded-md hover:bg-gray-700 hover:transition hover:duration-300 hover:opacity-100"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-8 w-8"
+                fill="white"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <rect
+                  x="3"
+                  y="3"
+                  width="8"
+                  height="8"
+                  stroke="currentColor"
+                  stroke-width="2"
+                />
+                <rect
+                  x="15"
+                  y="3"
+                  width="8"
+                  height="8"
+                  stroke="currentColor"
+                  stroke-width="2"
+                />
+                <rect
+                  x="3"
+                  y="15"
+                  width="8"
+                  height="8"
+                  stroke="currentColor"
+                  stroke-width="2"
+                />
+                <rect
+                  x="15"
+                  y="15"
+                  width="8"
+                  height="8"
+                  stroke="currentColor"
+                  stroke-width="2"
+                />
+              </svg>
+              <p class="mx-4 text-[18px]">LEARN</p>
+            </router-link>
+          </li>
+        </div>
+        <div>
+          <li class="">
+            <router-link
+              to="/project"
+              @click="handleClose"
+              active-class="sidebar-link-active"
+              class="flex flex-row items-center px-4 py-2  rounded-md hover:bg-gray-700 hover:transition hover:duration-300 hover:opacity-100"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-8 w-8"
+                fill="white"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <rect
+                  x="3"
+                  y="3"
+                  width="8"
+                  height="8"
+                  stroke="currentColor"
+                  stroke-width="2"
+                />
+                <rect
+                  x="15"
+                  y="3"
+                  width="8"
+                  height="8"
+                  stroke="currentColor"
+                  stroke-width="2"
+                />
+                <rect
+                  x="3"
+                  y="15"
+                  width="8"
+                  height="8"
+                  stroke="currentColor"
+                  stroke-width="2"
+                />
+                <rect
+                  x="15"
+                  y="15"
+                  width="8"
+                  height="8"
+                  stroke="currentColor"
+                  stroke-width="2"
+                />
+              </svg>
+              <p class="mx-4 text-[18px]">PROJECT</p>
+            </router-link>
+          </li>
+        </div>
+        <div>
+          <li class="">
+            <router-link
+              to="/profile"
+              active-class="sidebar-link-active"
+              @click="handleClose"
+              class="flex flex-row items-center px-4 py-2  rounded-md hover:bg-gray-700 hover:transition hover:duration-300 hover:opacity-100"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-8 w-8"
+                fill="white"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <rect
+                  x="3"
+                  y="3"
+                  width="8"
+                  height="8"
+                  stroke="currentColor"
+                  stroke-width="2"
+                />
+                <rect
+                  x="15"
+                  y="3"
+                  width="8"
+                  height="8"
+                  stroke="currentColor"
+                  stroke-width="2"
+                />
+                <rect
+                  x="3"
+                  y="15"
+                  width="8"
+                  height="8"
+                  stroke="currentColor"
+                  stroke-width="2"
+                />
+                <rect
+                  x="15"
+                  y="15"
+                  width="8"
+                  height="8"
+                  stroke="currentColor"
+                  stroke-width="2"
+                />
+              </svg>
+              <p class="mx-4 text-[18px]">PROFILE</p>
+            </router-link>
+          </li>
+        </div>
+        <div>
+          <li class="">
+            <router-link
+              to="/support"
+              @click="handleClose"
+              active-class="sidebar-link-active"
+              class="flex flex-row items-center px-4 py-2  rounded-md hover:bg-gray-700 hover:transition hover:duration-300 hover:opacity-100"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-8 w-8"
+                fill="white"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <rect
+                  x="3"
+                  y="3"
+                  width="8"
+                  height="8"
+                  stroke="currentColor"
+                  stroke-width="2"
+                />
+                <rect
+                  x="15"
+                  y="3"
+                  width="8"
+                  height="8"
+                  stroke="currentColor"
+                  stroke-width="2"
+                />
+                <rect
+                  x="3"
+                  y="15"
+                  width="8"
+                  height="8"
+                  stroke="currentColor"
+                  stroke-width="2"
+                />
+                <rect
+                  x="15"
+                  y="15"
+                  width="8"
+                  height="8"
+                  stroke="currentColor"
+                  stroke-width="2"
+                />
+              </svg>
+              <p class="mx-4 text-[18px]">SUPPORT</p>
+            </router-link>
+          </li>
+        </div>
+      </ul>
+    </div>
+  </transition>
+</template>
+
+<style>
+.slide-enter-active,
+.slide-leave-active {
+  transition: transform 0.3s ease;
+}
+
+.slide-enter-from,
+.slide-leave-to {
+  transform: translateX(-100%);
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.2 ease-out;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.sidebar-link-active {
+  background-color: #29374a;
+  font-weight: bold;
+}
+</style>
